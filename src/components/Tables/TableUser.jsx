94c9742734a5c21b7122ecdd2c316a8e1aea1a2f
Densovan <sarimramata@gmail.com>
2024-08-09 08:30:37 +0700

    add user modal

--- src/components/Tables/TableUser.jsx
+++ src/components/Tables/TableUser.jsx
@@ -68,14 +68,49 @@ const TableUser = () => {
       </div>
 
       <AddUser isOpen={isModalOpen} onClose={closeModal}>
-        <h2 className="text-lg font-bold">Modal Content</h2>
-        <p className="mt-2">This is the content inside the modal.</p>
-        <button
+        <form class="max-w-sm mx-auto">
+          <div class="mb-5">
+            <label
+              for="email"
+              class="block mb-2 text-sm font-medium text-gray-900 "
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              for="password"
+              className="block mb-2 text-sm font-medium text-gray-900 "
+            >
+              Your Userame
+            </label>
+            <input
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+
+        {/* <p className="mt-2">This is the content inside the modal.</p> */}
+        {/* <button
           onClick={closeModal}
           className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
         >
           Close Modal
-        </button>
+        </button> */}
       </AddUser>
 
       <div className="flex flex-col">
